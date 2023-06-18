c8e13224b0449d54af10562ece90734e589421dc
rockriviera <isak.shamun@gmail.com>
2023-06-18 18:35:05 +0200

    conform to js style conventions

--- src/Main.js
+++ src/Main.js
@@ -3,29 +3,25 @@ input=fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
 
 let instructions = input.split(',').map(Number);
 //Changing values in index 1 and 2 according to AoC instructions.
-instructions[1] = 12
-instructions[2] = 2
-//opcode default value
-opcode = 0
+instructions[1] = 12;
+instructions[2] = 2;
 for (let i = 0; i < instructions.length; i += 4) {
-    let opcode = instructions[i]
+    let opcode = instructions[i];
     let value1 = instructions[instructions[i + 1]];
     let value2 = instructions[instructions[i + 2]];
     let outputPosition = instructions[i + 3];
     switch (opcode) {
-        //Sums two sequential values and outputs result to index outputPosition
         case 1:
-            sum = value1 + value2
-            instructions[outputPosition] = sum
+            sum = value1 + value2;
+            instructions[outputPosition] = sum;
             break;
         case 2:
-            //Multiplies two sequential values and outputs result to index outputPosition
-            product = value1 * value2
-            instructions[outputPosition] = product
+            product = value1 * value2;
+            instructions[outputPosition] = product;
             break;
         case 99:
-            return console.log(`Opcode 99 detected,stopping program.\nResult:${instructions[0]}`)
+            return console.log(`Opcode 99 detected,stopping program.\nResult:${instructions[0]}`);
         default:
-            return console.log(`Error: Unexpected opcode in index ${i}`)
+            return console.log(`Error: Unexpected opcode in index ${i}`);
     }
 }
